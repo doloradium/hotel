d9ba09cba3d0a0a0750a5ebdd255bbb0cd1605dc
Valery Makey <valerymakkol@gmail.com>
2025-03-22 23:25:15 +0300

    Fixed favicon for Safari

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
-import logo from '/logo.svg';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
 
+import logo from '@/assets/logo.svg';
 import ModalHistory from '@/components/ModalHistory';
 
 export default function Footer() {
