ce6f9235935f192d83ae4cafc1f40f783395df78
Valery Makey <valerymakkol@gmail.com>
2025-03-29 00:54:02 +0300

    Fixed main inputs

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import group8 from '@/assets/Group 8.svg';
 import heroImage from '@/assets/hero.jpg';
+import Button from '@/components/Button';
 import CardRoom from '@/components/CardRoom';
 
 import { FeaturesSelection, RatingFilter } from './Login';
@@ -14,6 +15,7 @@ export default function Main() {
   const [showEndCalendar, setShowEndCalendar] = useState(false);
   const [showAmenities, setShowAmenities] = useState(false);
   const [selectedAmenities, setSelectedAmenities] = useState<string[]>([]);
+  const [isHovered, setIsHovered] = useState(false);
 
   const features = [
     "Шумоизоляция",
@@ -55,119 +57,71 @@ export default function Main() {
     <div className="min-h-screen">
       <div style={{ backgroundImage: `url(${heroImage})` }} className={`w-full relative h-120 sm:h-96 bg-cover bg-center p-4 mb-60 sm:mb-32`}>
         <div className="bg-white p-4 rounded-2xl shadow-(--custom-shadow) w-[calc(100%-2rem)] max-w-7xl absolute top-full left-1/2 -translate-1/2">
-          <div className="flex flex-col gap-2 sm:flex-row sm:gap-4">
-            <div className="flex flex-col flex-1 gap-2">
-              <div className="flex flex-col gap-2 sm:flex-row sm:gap-4 md:items-end">
-                <div className="w-full sm:w-auto">
-                  <div className="mb-1 text-xs text-gray-500 sm:text-sm">Стоимость за сутки</div>
-                  <div className="flex flex-row gap-2 sm:gap-4">
-                    <input
-                      type="number"
-                      placeholder="от 0"
-                      className="w-full sm:w-[160px] lg:w-[200px] h-9 sm:h-12 border border-gray-200 px-2 sm:px-4 rounded-xl bg-gray-50 text-gray-400 placeholder-gray-400 text-sm"
-                      value={minPrice}
-                      onChange={(e) => setMinPrice(Number(e.target.value))}
-                    />
-                    <input
-                      type="number"
-                      placeholder="до 55 000"
-                      className="w-full sm:w-[160px] lg:w-[200px] h-9 sm:h-12 border border-gray-200 px-2 sm:px-4 rounded-xl bg-gray-50 text-gray-400 placeholder-gray-400 text-sm"
-                      value={maxPrice}
-                      onChange={(e) => setMaxPrice(Number(e.target.value))}
-                    />
-                  </div>
-                </div>
+          <div className="grid grid-cols-8 gap-1">
+            <div className="col-span-2 mb-1 text-xs text-gray-500 sm:text-sm">Стоимость за сутки</div>
+            <div className="col-span-2 mb-1 text-xs text-gray-500 sm:text-sm">Даты пребывания</div>
+            <div className="col-span-2 mb-1 text-xs text-gray-500 sm:text-sm">Количество человек</div>
+            <div className="col-span-2"></div>
 
-                <div className="w-full sm:w-auto">
-                  <div className="mb-1 text-xs text-gray-500 sm:text-sm">Даты пребывания</div>
-                  <div className="flex flex-row gap-2 sm:gap-4">
-                    <div className="relative flex-1 w-full sm:w-auto sm:flex-none">
-                      <input
-                        type="text"
-                        placeholder="Заезд"
-                        className="w-full sm:w-[140px] lg:w-[160px] h-9 sm:h-12 border border-gray-200 px-2 sm:px-4 rounded-xl bg-gray-50 text-gray-400 placeholder-gray-400 cursor-pointer text-sm"
-                        value={''}
-                        onClick={() => setShowStartCalendar(!showStartCalendar)}
-                        readOnly
-                      />
-                    </div>
-                    <div className="relative flex-1 w-full sm:w-auto sm:flex-none">
-                      <input
-                        type="text"
-                        placeholder="Выезд"
-                        className="w-full sm:w-[140px] lg:w-[160px] h-9 sm:h-12 border border-gray-200 px-2 sm:px-4 rounded-xl bg-gray-50 text-gray-400 placeholder-gray-400 cursor-pointer text-sm"
-                        value={''}
-                        onClick={() => setShowEndCalendar(!showEndCalendar)}
-                        readOnly
-                      />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="w-full sm:w-auto">
-                  <div className="mb-1 text-xs text-gray-500 sm:text-sm">Количество человек</div>
-                  <input
-                    type="number"
-                    placeholder="1"
-                    className="w-full sm:w-[100px] lg:w-[120px] h-9 sm:h-12 border border-gray-200 px-2 sm:px-4 rounded-xl bg-gray-50 text-gray-400 placeholder-gray-400 text-sm"
-                  />
-                </div>
-              </div>
-
-              <div className="flex flex-col gap-2 sm:flex-row sm:gap-4">
-                <div className="relative w-full">
-                  <div
-                    className="flex items-center w-full px-2 text-sm text-gray-400 border border-gray-200 cursor-pointer h-9 sm:h-12 sm:px-4 rounded-xl bg-gray-50"
-                    onClick={() => setShowAmenities(!showAmenities)}
-                  >
-                    {selectedAmenities.length ? `Выбрано: ${selectedAmenities.length}` : 'Удобства'}
-                  </div>
-
-                  {showAmenities && (
-                    <div className="absolute left-0 w-full p-3 bg-white rounded-lg shadow-lg top-10 sm:top-12">
-                      <div className="flex flex-col gap-2">
-                        {features.map((feature, index) => (
-                          <label key={index} className="flex items-center gap-2 cursor-pointer">
-                            <input
-                              type="checkbox"
-                              checked={selectedAmenities.includes(feature)}
-                              onChange={() => {
-                                if (selectedAmenities.includes(feature)) {
-                                  setSelectedAmenities(selectedAmenities.filter(f => f !== feature));
-                                } else {
-                                  setSelectedAmenities([...selectedAmenities, feature]);
-                                }
-                              }}
-                              className="border-gray-300 rounded"
-                            />
-                            <span className="text-sm text-gray-600">{feature}</span>
-                          </label>
-                        ))}
-                      </div>
-                    </div>
-                  )}
-                </div>
-
-                <div className="w-full">
-                  <select
-                    value={rating}
-                    onChange={(e) => setRating(Number(e.target.value))}
-                    className="w-full px-2 text-sm text-gray-400 border border-gray-200 h-9 sm:h-12 sm:px-4 rounded-xl bg-gray-50"
-                  >
-                    <option value={0}>Рейтинг</option>
-                    {[1, 2, 3, 4, 5].map((star) => (
-                      <option key={star} value={star}>{star} ★</option>
-                    ))}
-                  </select>
-                </div>
-              </div>
-            </div>
-
-            <div className="mt-2 sm:mt-0 sm:ml-2 md:ml-4">
-              <button className="w-full sm:w-[140px] h-full min-h-[76px] bg-[#4086F6] hover:bg-[#3575E2] text-white font-normal text-base rounded-xl transition-colors duration-200 flex items-center justify-center">
+            <input
+              type="number"
+              placeholder="от 0"
+              className={`w-full h-12 px-4 text-sm text-gray-400 placeholder-gray-400 border border-gray-200 bg-gray-50 col-span-1 transition-all duration-300 rounded-sm rounded-tl-2xl`}
+              value={minPrice}
+              onChange={(e) => setMinPrice(Number(e.target.value))}
+            />
+            <input
+              type="number"
+              placeholder="до 55 000"
+              className={`w-full h-12 px-4 text-sm text-gray-400 placeholder-gray-400 border border-gray-200 bg-gray-50 col-span-1 transition-all duration-300 rounded-sm`}
+              value={maxPrice}
+              onChange={(e) => setMaxPrice(Number(e.target.value))}
+            />
+            <input
+              type="text"
+              placeholder="Заезд"
+              className={`w-full h-12 px-4 text-sm text-gray-400 placeholder-gray-400 border border-gray-200 cursor-pointer bg-gray-50 col-span-2 transition-all duration-300 rounded-sm`}
+              value={''}
+              readOnly
+            />
+            <input
+              type="text"
+              placeholder="Выезд"
+              className={`w-full h-12 px-4 text-sm text-gray-400 placeholder-gray-400 border border-gray-200 cursor-pointer bg-gray-50 col-span-2 transition-all duration-300 ${isHovered ? 'rounded-sm rounded-tr-2xl' : 'rounded-sm'}`}
+              value={''}
+              readOnly
+            />
+            <div
+              className="col-span-2 row-span-2"
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              <Button className="w-full h-full rounded-sm hover:rounded-2xl rounded-tr-2xl rounded-br-2xl">
                 Найти
-              </button>
+              </Button>
             </div>
+
+            <input
+              type="number"
+              placeholder="1"
+              className={`w-full h-12 px-4 text-sm text-gray-400 placeholder-gray-400 border border-gray-200 bg-gray-50 col-span-2 transition-all duration-300 rounded-sm rounded-bl-2xl`}
+            />
+            <div
+              className={`flex items-center w-full h-12 px-4 text-sm text-gray-400 border border-gray-200 cursor-pointer bg-gray-50 col-span-2 transition-all duration-300 rounded-sm`}
+              onClick={() => setShowAmenities(!showAmenities)}
+            >
+              {selectedAmenities.length ? `Выбрано: ${selectedAmenities.length}` : 'Удобства'}
+            </div>
+            <select
+              value={rating}
+              onChange={(e) => setRating(Number(e.target.value))}
+              className={`w-full h-12 px-4 text-sm text-gray-400 border border-gray-200 bg-gray-50 col-span-2 transition-all duration-300 ${isHovered ? 'rounded-sm rounded-br-2xl' : 'rounded-sm'}`}
+            >
+              <option value={0}>Рейтинг</option>
+              {[1, 2, 3, 4, 5].map((star) => (
+                <option key={star} value={star}>{star} ★</option>
+              ))}
+            </select>
           </div>
         </div>
       </div>
